718e8395b455717642fee1294f7478fa4951245b
sambeltran <sambeltranesco20@gmail.com>
2017-12-22 11:51:38 -0500

    subo los ultimos cambios

---
+++ js/wallomjscript.js
@@ -0,0 +1,9 @@
+/**window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("own-nav").style.top = "0";
+    } else {
+        document.getElementById("own-nav").style.top = "-50px";
+    }
+}**/
